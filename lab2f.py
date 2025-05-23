5f3ba36cdd5126b9f20e1300ac1d2bf3f64d4f5e
Urmil patel <urpatel5@myseneca.ca>
2025-05-23 00:00:54 -0400

    Added lab2f

---
+++ lab2f.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# Author: Urmil Rohitkumar Patel
+# Author ID: urpatel5
+# Date Created: 2025/05/22
+
+import sys
+
+timer = int(sys.argv[1])
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print("blast off!")
+
